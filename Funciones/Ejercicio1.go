494285fb856f8b037a1f46d6b4a49257f6bcfa00
Maznell <cuevaenzo@gmail.com>
2023-04-08 00:47:31 -0300

    primer commit

---
+++ Funciones/Ejercicio1.go
@@ -0,0 +1,4 @@
+//Definir una función que, dado los coeficientes de un polinomio de grado n (números flotantes), muestre por pantalla el polinomio completo,
+//por ejemplo si recibe los coeficientes 3.0, 2.0 y 1.0 debe mostrar 3.0 + 2.0 X + 1.0 X**2
+
+package funciones
---
+++ Funciones/Ejercicio2.go
@@ -0,0 +1,7 @@
+// 2. Formar un menú con 4 opciones (como se muestra debajo) y al elegir una de ellas mostrar un cartel diciendo qué opción se eligió o si fue una opción incorrecta.
+// - Opción 1
+// - Opción 2
+// - Opción 3
+// - Opción 4
+
+package funciones
